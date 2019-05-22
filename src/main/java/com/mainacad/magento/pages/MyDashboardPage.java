0de820172e08dfdd4616a2f9b282a909595bbbaa
Andrii Heha <aheha@playtika.com>
2019-05-22 16:59:34 +0300

    Here is My draft magento Project

---
+++ src/main/java/com/mainacad/magento/pages/MyDashboardPage.java
@@ -0,0 +1,11 @@
+package com.mainacad.magento.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class MyDashboardPage extends BasePage {
+
+    //*********Constructor*********
+    public MyDashboardPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/main/java/com/mainacad/magento/pages/RegistrationPage.java
@@ -0,0 +1,14 @@
+package com.mainacad.magento.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegistrationPage extends BasePage{
+
+    //*********Constructor*********
+    public RegistrationPage(WebDriver driver) {
+        super(driver);
+    }
+
+    //*********Web Elements*********
+}
+
